229685c6f8cd4ad383e49ad9a347909d6d6a891e
Nhan Nguyen <nhan2151@gmail.com>
2024-12-28 13:57:09 +0700

    add two routes for run

--- src/main/java/dev/nhan/demo/run/RunController.java
+++ src/main/java/dev/nhan/demo/run/RunController.java
@@ -1,18 +1,30 @@
 package dev.nhan.demo.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
-    private List<Run>  runs = new ArrayList<>();
+    private final RunRepository runRepository;
 
-    @GetMapping("/hello")
-    String home() {
-        return "Hello, World!";
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    @GetMapping("")
+    List<Run> findAll() {
+       return runRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        return runRepository.findById(id);
     }
 }
--- src/main/java/dev/nhan/demo/run/RunRepository.java
+++ src/main/java/dev/nhan/demo/run/RunRepository.java
@@ -1,4 +1,43 @@
 package dev.nhan.demo.run;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class RunRepository {
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Run findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst()
+                .get();
+    }
+
+    @PostConstruct // Class created this message will be called
+    private void init() {
+        runs.add(new Run(
+                1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                5,
+                Location.OUTDOOR));
+        runs.add(new Run(
+                2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+                6,
+                Location.INDOOR));
+    }
 }
