3ec44e79245c2c50b21414e005f2b8874de22bec
Nhan Nguyen <nhan2151@gmail.com>
2024-12-28 16:42:49 +0700

    Add full route for run app

--- src/main/java/dev/nhan/demo/run/RunController.java
+++ src/main/java/dev/nhan/demo/run/RunController.java
@@ -1,12 +1,11 @@
 package dev.nhan.demo.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -20,11 +19,38 @@ public class RunController {
 
     @GetMapping("")
     List<Run> findAll() {
-       return runRepository.findAll();
+        return runRepository.findAll();
     }
 
     @GetMapping("/{id}")
     Run findById(@PathVariable Integer id) {
-        return runRepository.findById(id);
+        Optional<Run> run = runRepository.findById(id);
+
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+        }
+
+        return run.get();
+    }
+
+    // post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    // put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
+
+    // delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
     }
 }
